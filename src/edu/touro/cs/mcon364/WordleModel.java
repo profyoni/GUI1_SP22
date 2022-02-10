0816ea349162b8c3e85535ecb1572281ce124609
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2022-02-10 12:24:10 -0500

    v2

--- src/edu/touro/cs/mcon364/WordleModel.java
+++ src/edu/touro/cs/mcon364/WordleModel.java
@@ -1,5 +1,29 @@
 package edu.touro.cs.mcon364;
 
+import java.util.List;
+
+enum WordleResponse {CORRECT, WRONG, WRONG_POSITION}
 public class WordleModel {
+
+    String target;
+    List<String> wordList;
+    //sets wordList reads file once
+    public WordleModel()
+    {
+    }
+    /**
+     * reset target
+     */
+    public void newGame(){}
+
+    /**
+     *
+     * @param s
+     * @return List of 5 entries if legal. empty list or something else  if not legal word
+     */
+    public List<WordleResponse> checkGuess(String s)
+    {
+        return null;
+    }
 
 }
